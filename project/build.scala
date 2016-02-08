3eaabcb01361ad7d865065c201803b9458891db4
tek <torstenschmits@gmail.com>
2016-02-08 13:48:35 +0100

    add publishing config

--- project/build.scala
+++ project/build.scala
@@ -2,14 +2,22 @@ package tryp
 
 import sbt._, Keys._
 
+import bintray.BintrayKeys._
+
 object Build
 extends LibsBuild("speclectic", deps = SDeps)
 {
   override lazy val root = (
-    "root" ~ "." settingsV(
-      fork := true,
-      parallelExecution := false, 
-      javaOptions in Test ++= Seq("-XX:+CMSClassUnloadingEnabled")
-    ) !
+    ("root" ~ ".")
+      .bintray
+      .settingsV(
+        name := "speclectic",
+        fork := true,
+        licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
+        bintrayRepository := "releases",
+        publishArtifact in (Compile, packageDoc) := false,
+        publishArtifact in (Compile, packageSrc) := false
+      )
+      .!
   )
 }
